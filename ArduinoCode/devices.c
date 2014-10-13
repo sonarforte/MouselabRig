2eaf2ec214c8c0a990a8a5144748a5956fe802ed
Steven Rofrano <srofrano@gmail.com>
2014-10-12 22:00:21 -0400

    Deleted Dsstore

---
+++ ArduinoCode/devices.c
@@ -0,0 +1,39 @@
+  // devices.c
+// Defines device interface functions 
+// Steven Rofrano
+// 2014-07-28
+
+#include "devices.h"
+
+
+volatile int valveOff;
+extern volatile int valveState;
+
+// Opens valve and sets time to close it
+unsigned long valveOpen( int ms ) {
+
+	// digitalWriteFast(LED_PIN, HIGH);
+	digitalWriteFast(VALVE_PIN, HIGH);
+	unsigned long timeOff = millis() + ms;
+	valveState = 1;
+	return timeOff;
+
+}
+
+
+void valveClose( void ) {
+	// digitalWriteFast(LED_PIN, LOW);
+	digitalWriteFast(VALVE_PIN, LOW);
+	valveState = 0;
+
+}
+
+// Resets the Arduino when called through the watchdog timer
+void reset( void ) {
+
+	cli();
+	wdt_enable(WDTO_500MS);
+	while(1);
+	sei();
+
+}
---
+++ ArduinoCode/devices.h
@@ -0,0 +1,40 @@
+// devices.h
+// Header file for devices.c 
+// Steven Rofrano
+// 2014-07-28
+
+
+
+#ifndef DEVICES_H_	// continues only if the header has not yet been included
+#define DEVICES_H_
+
+#include <Arduino.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include "digitalWriteFast.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#define PHOTO_PIN 	5
+#define VALVE_PIN 	6
+#define OPT_CH1_PIN	4
+#define OPT_CHA_PIN	2
+#define OPT_CHB_PIN	3
+#define LED_PIN 	13		// built-in LED on pin 13		
+
+#define A02			500
+#define C02			100
+
+// Opens valve and sets closing time ms milliseconds later
+unsigned long valveOpen( int ms );
+
+// Closes the valve (called from main() after valveOff time has expired)
+void valveClose( void ); 
+
+// Resets the Arduino from software (should only be called from Pi at program start)
+void reset( void );
+
+
+
+#endif
